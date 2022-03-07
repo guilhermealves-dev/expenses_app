58c5572e9602fdbafe3ba89deddcf7acf04af9b7
Guilherme Alves <guilherme_alves@outlook.com.br>
2022-03-07 02:22:44 -0300

    finalization of the main functionalities of the application

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double, DateTime) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+
+  _submitForm() {
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0 || _selectedDate == null) {
+      return;
+    }
+    widget.onSubmit(title, value, _selectedDate);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              controller: _valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+              onSubmitted: (_) => _submitForm(),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    _selectedDate == null
+                        ? 'Nenhuma data selecionada!'
+                        : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}',
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: Text(
+                    'Selecionar Data',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: _showDatePicker,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  child: Text('Nova Transação'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.primary,
+                    textStyle: const TextStyle(
+                        //color: Theme.of(context).textTheme.button.color,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: _submitForm,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  TransactionList(this.transactions, this.onRemove);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 430,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  'Nenhuma transação cadastrada!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              //only render item list when this is visible on screen
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+                return Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            'R\$${tr.value}',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => onRemove(tr.id),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
